384db1dcd781a418ed19e8ef575f2eefd7d4bceb
sorayanfurter <sorinfurter@gmail.com>
2023-08-11 12:56:29 -0300

    Cross Origin JWTrequest

--- src/main/java/com/sistema/examenes/entidades/JwtRequest.java
+++ src/main/java/com/sistema/examenes/entidades/JwtRequest.java
@@ -1,8 +1,10 @@
 package com.sistema.examenes.entidades;
 
 import lombok.Data;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Data
+@CrossOrigin("https://portal-examenes.web.app/")
 
 public class JwtRequest {
     private String username;
